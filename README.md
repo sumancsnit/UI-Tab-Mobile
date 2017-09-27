bb2e5b9678abb101ecc944959cdd528f666417d6
Suman Kumar <suman@Nehas-MacBook-Air.local>
2017-09-27 21:49:27 +0530

    add customized bootstrap

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # WsUItest
 
+Demo: http://rawgit.com/sumancsnit/WsUItest/master/index.html
+
 One Section Left, will finish it tonight.
 
 Suman Kumar
--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,6 @@
 /*-------------Written By Suman Kumar * sumancsnit@gmail.com --------------------*/
 
-// Hide menu after click mobile
+// Hide menu after click -- sm
 $(function () {
     $('.navbar-collapse ul li a').on("click touch", function () {
         $('.navbar-toggle').click();
