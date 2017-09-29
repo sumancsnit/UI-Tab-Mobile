7c57d9328027be4556d64b6749854da8d222d4b4
Suman Kumar <suman@Nehas-MacBook-Air.local>
2017-09-29 11:49:11 +0530

    add tab section

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,17 @@
 /*-------------Written By Suman Kumar * sumancsnit@gmail.com --------------------*/
 
-// Hide menu after click -- sm
 $(function () {
     $('.navbar-collapse ul li a').on("click touch", function () {
         $('.navbar-toggle').click();
     });
+
+    $('.navbar-toggle').on("click touch", function () {
+
+        console.log(this);
+        if (this.className.toLowerCase().indexOf('collapsed') === -1) {
+            $(this).children().addClass('collapsed-color');
+        } else {
+            $(this).children().removeClass('collapsed-color')
+        }
+    });
 });
-
-
